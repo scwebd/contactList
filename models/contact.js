6ebe1b115512db91ee7d00f851f7e7f038d627e4
scwebd <scwebd@gmail.com>
2018-03-08 23:03:50 -0700

    Initial commit of solution

---
+++ models/contact.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+    var Contact = sequelize.define("Contact", {
+        // firstName (VARCHAR, NOT NULL, between 1-100 characters)
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 100]
+            }
+        },
+        // lastName (VARCHAR, NOT NULL, between 1-100 characters)
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 100]
+            }
+        },
+        // contactType (VARCHAR, Default value "Personal")       
+        contactType: {
+            type: DataTypes.STRING,
+            defaultValue: "Personal"
+        },
+        // phoneNumber (VARCHAR, length 10 characters, numbers only)
+        phoneNumber: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [10, 10],
+                is: ["^[0-9]+$",'i']
+            }
+        },
+        // emailAddress (VARCHAR, must be valid email format)
+        emailAddress: {
+            type: DataTypes.STRING,
+            validate: {
+                isEmail: true
+            }
+        }
+    });
+    // be sure to return the model!
+    return Contact;
+};
---
+++ routes/api-routes.js
@@ -0,0 +1,46 @@
+var db = require("../models");
+
+module.exports = function(app) {
+	app.get("/", function(req, res) {
+		// this route should find all contacts in the table and render them using the Handlebars 'contacts' template, sorted ascending by firstName
+		db.Contact.findAll({
+			order: [['firstName', 'ASC'], ['lastName', 'ASC']]
+		}).then(function(contacts) {
+			res.render("contacts", { name: "Sarah", contacts: contacts });
+		});
+	});
+
+	app.get("/api/contacts", function(req, res) {
+		// this route should find all contacts in the table and display them as JSON
+		db.Contact.findAll({}).then(function(contacts) {
+			res.json(contacts);
+		});
+	});
+
+	app.post("/api/contacts", function(req, res) {
+		// this route should add a new contact to the table, and should then redirect to the route '/api/contacts'
+		db.Contact.create({
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			contactType: req.body.contactType,
+			phoneNumber: req.body.phoneNumber,
+			emailAddress: req.body.emailAddress
+		}).then(function(newContact) {
+			console.log("New contact:");
+			console.log(newContact);
+			res.redirect("/api/contacts");
+		});
+	});
+
+	app.delete("/api/contacts/:id", function(req, res) {
+		// this route should delete a contact from the table, if the id matches the ':id' url param
+		db.Contact.destroy({
+			where: {
+				id: req.params.id
+			}
+		}).then(function(contact) {
+			console.log(contact);
+			res.json(contact);
+		});
+	});
+}
