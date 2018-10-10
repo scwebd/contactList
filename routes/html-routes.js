cc0f22656d61b62383e8b542acc6ef7cf664e4bb
scwebd <scwebd@gmail.com>
2018-10-10 17:20:40 -0600

    Fixing tiny commenting inconsistency

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,8 @@
 var path = require("path");
 
 module.exports = function(app) {
+    // this route should render the Handlebars 'form' template
 	app.get("/contacts/new", function(req, res) {
-		// this route should render the Handlebars 'form' template
 		res.render("form");
 	});
 }
